79053dd999a304921a907607e794f38bcfcc5169
algedi <andrea.calvia@gmail.com>
2024-08-29 16:13:13 +0000

    nvim_lazy_setup gitlab => github

--- app/nvim/config.sh
+++ app/nvim/config.sh
@@ -5,4 +5,4 @@ rm -rf $HOME/.cache/nvim
 rm -rf $HOME/.local/share/nvim
 rm -rf $HOME/.local/state/nvim
 mkdir -p $HOME/.config/nvim
-git clone https://gitlab.com/kui.team/nvim_lazy_setup.git $HOME/.config/nvim
+git clone https://github.com/algedi/nvim_lazy_setup.git $HOME/.config/nvim
